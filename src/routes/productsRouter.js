eca0af8161a076faed32c9ecf8df8b2ca04c2fa4
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-23 15:38:06 -0300

    feat: add the post products route

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -3,6 +3,7 @@ import {
   getFilteredProducts,
   getProducts,
   getSelectedProduct,
+  postProduct,
 } from "../controllers/productsController.js";
 
 const productsRouter = Router();
@@ -10,5 +11,5 @@ const productsRouter = Router();
 productsRouter.get("/products", getProducts);
 productsRouter.get("/products/:idProduct", getSelectedProduct);
 productsRouter.get("/filter_products/:name", getFilteredProducts);
-
+productsRouter.post("/producs", postProduct);
 export default productsRouter;
