03c98008ceaec394bffaef6f8ebdcd2c9d61cfac
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-23 15:01:19 -0300

    feat: add the getFilteredProducts controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -8,3 +8,14 @@ export default async function getProducts(req, res) {
     return res.status(500).send({ message: "Server error" });
   }
 }
+
+export default async function getFilteredProducts(req, res){
+   const name = req.params.name;
+   try{
+    const filteredProducts = await productsCollection.find({name:{$regex:name, $options:"i"}}).toArray();
+    return res.status(200).send({  filteredProducts });
+   }catch(err){
+    return res.status(500).send({ message: "Server error" });
+   }
+   
+}
