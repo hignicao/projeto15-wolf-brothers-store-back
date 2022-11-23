5d419ba55813247c77c45c0ab77b3533e52028e5
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-23 15:18:00 -0300

    feat: add the get products/:idProducts route

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getFilteredProducts, getProducts } from "../controllers/productsController.js";
-
+import {
+  getFilteredProducts,
+  getProducts,
+  getSelectedProduct,
+} from "../controllers/productsController.js";
 
 const productsRouter = Router();
 
-productsRouter.get("/products",getProducts);
-
-productsRouter.get("/filter_products/:name",getFilteredProducts);
+productsRouter.get("/products", getProducts);
+productsRouter.get("/products/:idProduct", getSelectedProduct);
+productsRouter.get("/filter_products/:name", getFilteredProducts);
