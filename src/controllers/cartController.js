e8431de326b0ad5e61c4f75d2cc6bc5faa0a367c
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-24 05:24:30 -0300

    feat: add the getProductsInTheCart function

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -3,13 +3,20 @@ import { cartCollection } from "../database/db.js";
 export async function postProductToCart(req, res) {
   const product = req.product;
   const user = req.user;
-  console.log(user, product)
   try {
-    console.log("entrei")
-    await cartCollection.insertOne({...product, key: user._id });
+    await cartCollection.insertOne({ ...product, key: user._id });
     return res.sendStatus(200);
   } catch (err) {
-    console.log('fudeeo')
+    return res.status(500).send({ message: "Server error" });
+  }
+}
+
+export async function getProductsInTheCart() {
+  const user = req.user;
+  try{
+      const products = await cartCollection.find({key:user._id}).toArray();
+      return res.send(products);
+  }catch(err){
     return res.status(500).send({ message: "Server error" });
   }
 }
