97c33fa52cda829de224e1b5ee5f6479c784f104
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-24 22:50:21 -0300

    feat:add the deleProductFromCartValidation function

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -6,15 +6,19 @@ import {
 } from "../controllers/cartController.js";
 import { addProductToCartValidation } from "../middlewares/addProductToCartValidationMiddleware.js";
 import { authValidation } from "../middlewares/authValidationMiddleware.js";
-
+import { deleteProductfromCartValidation } from "../middlewares/deleteProductFromCartValidationMiddleware.js";
 
 const cartRouter = Router();
 cartRouter.use(authValidation);
 cartRouter.post(
   "/cart/:productId",
- addProductToCartValidation,
+  addProductToCartValidation,
   postProductToCart
 );
 cartRouter.get("/cart", getProductsInTheCart);
-cartRouter.delete("/cart/:productId", deleteProductFromCart);
+cartRouter.delete(
+  "/cart/:productId",
+  deleteProductfromCartValidation,
+  deleteProductFromCart
+);
 export default cartRouter;
