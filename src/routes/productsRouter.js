fcb47064eb3b97940121810b70d7d91c6f27a0e6
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-26 18:34:30 -0300

    feat: add the getProductsBycategory

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   getFilteredProducts,
   getProducts,
+  getProductsByCategory,
   getSelectedProduct,
   postProduct,
 } from "../controllers/productsController.js";
@@ -12,5 +13,5 @@ productsRouter.get("/products", getProducts);
 productsRouter.get("/products/:idProduct", getSelectedProduct);
 productsRouter.get("/products/filter/:name", getFilteredProducts);
 productsRouter.post("/products", postProduct);
-
+productsRouter.get("/products/category/:type", getProductsByCategory)
 export default productsRouter;
