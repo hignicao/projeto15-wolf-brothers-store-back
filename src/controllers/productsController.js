b6e2c45ba61e66cd75fa07dc69835b4802924f04
João Vitor Feliciano <110498176+jvitorfeliciano@users.noreply.github.com>
2022-11-28 13:54:43 -0300

    Revert "Develop jv"

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,77 +2,68 @@ import { ObjectID } from "bson";
 import { productsCollection } from "../database/db.js";
 
 export async function getProducts(req, res) {
-  const page = Number(req.query.page);
-  const limit = 12;
-
-  try {
-    const array = await productsCollection.find().toArray();
-    const products = array.slice((page - 1) * limit, limit * page);
-
-    return res.status(200).send({ products, totalProducts:array.length });
-  } catch (err) {
-    return res.status(500).send({ message: "Server error" });
-  }
+	try {
+		const products = await productsCollection.find().toArray();
+		return res.status(200).send({ products });
+	} catch (err) {
+		return res.status(500).send({ message: "Server error" });
+	}
 }
 
 export async function getFilteredProducts(req, res) {
-  const name = req.params.name;
+	const name = req.params.name;
 
-  try {
-    const filteredProducts = await productsCollection
-      .find({ name: { $regex: name, $options: "i" } })
-      .toArray();
+	try {
+		const filteredProducts = await productsCollection.find({ name: { $regex: name, $options: "i" } }).toArray();
 
-    return res.status(200).send({ filteredProducts });
-  } catch (err) {
-    return res.status(500).send({ message: "Server error" });
-  }
+		return res.status(200).send({ filteredProducts });
+	} catch (err) {
+		return res.status(500).send({ message: "Server error" });
+	}
 }
 
 export async function getSelectedProduct(req, res) {
-  const idProduct = req.params.idProduct;
+	const idProduct = req.params.idProduct;
 
-  try {
-    const product = await productsCollection.findOne({
-      _id: ObjectID(idProduct),
-    });
-    return res.status(200).send({ product });
-  } catch (err) {
-    return res.status(500).send({ message: "Server error" });
-  }
+	try {
+		const product = await productsCollection.findOne({
+			_id: ObjectID(idProduct),
+		});
+		return res.status(200).send({ product });
+	} catch (err) {
+		return res.status(500).send({ message: "Server error" });
+	}
 }
 
 export async function postProduct(req, res) {
-  const product = req.body;
+	const product = req.body;
 
-  try {
-    await productsCollection.insertOne(product);
-    res.status(201).send({ message: "Successfully created" });
-  } catch (err) {
-    return res.status(500).send({ message: "Server error" });
-  }
+	try {
+		await productsCollection.insertOne(product);
+		res.status(201).send({ message: "Successfully created" });
+	} catch (err) {
+		return res.status(500).send({ message: "Server error" });
+	}
 }
 
 export async function getProductsByCategory(req, res) {
-  const { gender, type } = req.params;
+	const { gender, type } = req.params;
 
-  try {
-    const products = await productsCollection
-      .find({ $and: [{ type }, { gender }] })
-      .toArray();
-    return res.status(200).send(products);
-  } catch (err) {
-    return res.status(500).send({ message: "Server error" });
-  }
+	try {
+		const products = await productsCollection.find({ $and: [{ type }, { gender }] }).toArray();
+		return res.status(200).send(products);
+	} catch (err) {
+		return res.status(500).send({ message: "Server error" });
+	}
 }
 
 export async function deleteProd(req, res) {
-  try {
-    const id = req.params.id;
-    console.log(id);
-    await productsCollection.deleteOne({ _id: ObjectID(id) });
-    res.send("ok");
-  } catch (err) {
-    return res.send("f");
-  }
+	try {
+		const id = req.params.id;
+		console.log(id);
+		await productsCollection.deleteOne({ _id: ObjectID(id) });
+		res.send("ok");
+	} catch (err) {
+		return res.send("f");
+	}
 }
